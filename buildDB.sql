6e2658bf5d79faff752561298cc70a16495205ca
Evelina Fridmane <fridmane@kth.se>
2025-11-15 14:11:11 +0100

    Update buildDB.sql
    changed add part

--- buildDB.sql
+++ buildDB.sql
@@ -160,9 +160,6 @@ ALTER TABLE employee
 ALTER TABLE employee_activity
 	ADD CONSTRAINT fk_planned_activity_id
 	FOREIGN KEY (planned_activity_id) REFERENCES planned_activity(planned_activity_id) ON DELETE RESTRICT ON UPDATE RESTRICT
-ALTER TABLE department
-	ADD CONSTRAINT fk_department_employee
-	FOREIGN KEY (manager_id) REFERENCES employee(employment_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 ---------------------------------------------------------------------------------------------------------------------
 -- add triggers
