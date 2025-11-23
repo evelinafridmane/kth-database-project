f13db1b9f5b3dd5676b1842517600fe306a3be3e
Samia Serbouti <serbouti@kth.se>
2025-11-23 14:07:45 +0100

    Create query1.sql

---
+++ query1.sql
@@ -0,0 +1,3 @@
+%% Planned hours calculations: 
+%% Calculate the total hours (with the multiplication factor) along with the break-ups for each activity, for the current years’ course instances. 
+
