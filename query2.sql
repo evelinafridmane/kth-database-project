c2972d92a44d7f3e76dc435bd2b4e15d98899fae
Evelina Fridmane <fridmane@kth.se>
2025-11-23 18:52:28 +0100

    Create query2.sql

---
+++ query2.sql
@@ -0,0 +1,50 @@
+-- Calculate actual allocated hours for a course: 
+--Calculate the total allocated hours with the multiplication factor along with the break-ups 
+--for each activity and for each teacher, for a current years’ course instance. 
+
+
+
+-- counting the number of techers to then allocate equal amount of hours of the course to each teacher
+WITH teacher_counts AS (
+    SELECT pa.instance_id, 
+        COUNT(DISTINCT ea.employment_id) AS total_teachers
+    FROM planned_activity pa
+    JOIN employee_activity ea ON pa.planned_activity_id = ea.planned_activity_id
+    GROUP BY pa.instance_id
+)
+
+
+SELECT
+	cl.course_code AS "Course code",
+	ci.instance_id AS "Course instance ID",
+	cl.hp AS "HP",
+	p.first_name || ' ' || p.last_name AS "Teacher's Name",
+	jt.job_title as "Designation",
+	SUM(CASE WHEN ta.activity_name = 'Lecture' THEN pa.planned_nb_hours * ta.factor ELSE 0 END) AS "Lecture Hours",
+  	SUM(CASE WHEN ta.activity_name = 'Tutorial' THEN pa.planned_nb_hours * ta.factor ELSE 0 END) AS "Tutorial Hours",
+  	SUM(CASE WHEN ta.activity_name = 'Lab' THEN pa.planned_nb_hours * ta.factor ELSE 0 END) AS "Lab Hours",
+  	SUM(CASE WHEN ta.activity_name = 'Seminar' THEN pa.planned_nb_hours * ta.factor ELSE 0 END) AS "Seminar Hours",
+ 	SUM(CASE WHEN ta.activity_name = 'Overhead' THEN pa.planned_nb_hours * ta.factor ELSE 0 END) AS "Other Overhead Hours",
+  	MAX(2 * cl.hp + 28 + 0.2 * ci.num_students) / tc.total_teachers AS "Admin ",
+    MAX(32 + 0.725 * ci.num_students) / tc.total_teachers AS "Exam ",
+    
+    ( SUM(pa.planned_nb_hours * ta.factor) + 
+      (MAX(2 * cl.hp + 28 + 0.2 * ci.num_students) / tc.total_teachers) + 
+      (MAX(32 + 0.725 * ci.num_students) / tc.total_teachers)
+    ) AS "Total Hours"
+FROM
+  course_instance AS ci
+JOIN course_layout AS cl ON ci.course_layout_id = cl.course_layout_id
+JOIN planned_activity AS pa ON ci.instance_id = pa.instance_id
+JOIN teaching_activity AS ta ON pa.teaching_activity_id = ta.teaching_activity_id
+JOIN employee_activity AS ea ON pa.planned_activity_id = ea.planned_activity_id 
+JOIN employee AS e ON ea.employment_id = e.employment_id 
+JOIN job_title AS jt ON  e.job_id = jt.job_id 
+JOIN person AS p ON  e.person_id = p.person_id 
+JOIN teacher_counts AS tc ON ci.instance_id = tc.instance_id
+WHERE
+  ci.instance_id = 1
+  AND ci.study_year = EXTRACT(YEAR FROM CURRENT_DATE)
+  
+GROUP BY
+  cl.course_code, ci.instance_id, cl.hp, p.first_name, p.last_name, jt.job_title, tc.total_teachers;
